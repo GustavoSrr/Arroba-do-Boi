1a063501a7c9c473d0c580731b970e78aa6f9d8b
Gustavo <gustavosrodrigues2000@gmail.com>
2021-09-14 12:16:56 -0300

    Criando o componente Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Feito por <a href="https://github.com/GustavoSrr">Gustavo</a>
+      </p>
+    </footer>
+  );
+}
